672be484db6b6af003e5319681f431ef90374ccb
이건희 <022686s@gmail.com>
2024-10-28 13:57:47 +0900

    init project

---
+++ backend/me-to-you/src/main/java/nodawoon/me_to_you/MeToYouApplication.java
@@ -0,0 +1,13 @@
+package nodawoon.me_to_you;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MeToYouApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MeToYouApplication.class, args);
+	}
+
+}
---
+++ backend/me-to-you/src/test/java/nodawoon/me_to_you/MeToYouApplicationTests.java
@@ -0,0 +1,13 @@
+package nodawoon.me_to_you;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MeToYouApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
