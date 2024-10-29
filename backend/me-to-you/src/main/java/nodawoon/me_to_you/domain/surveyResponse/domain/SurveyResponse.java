a5718268ba4388fff5a45ffe6b0c60efa0c46c3c
조아름 <a_lme@naver.com>
2024-10-30 01:14:37 +0900

    feat: builder 추가 및 user table 연관관계 생성

--- backend/me-to-you/src/main/java/nodawoon/me_to_you/domain/surveyResponse/domain/SurveyResponse.java
+++ backend/me-to-you/src/main/java/nodawoon/me_to_you/domain/surveyResponse/domain/SurveyResponse.java
@@ -1,8 +1,10 @@
 package nodawoon.me_to_you.domain.surveyResponse.domain;
 
 import jakarta.persistence.*;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import nodawoon.me_to_you.domain.user.domain.User;
 import nodawoon.me_to_you.global.database.BaseEntity;
 
 import static jakarta.persistence.GenerationType.IDENTITY;
@@ -14,12 +16,14 @@ import static lombok.AccessLevel.PROTECTED;
 @Table(name = "survey_responses")
 public class SurveyResponse extends BaseEntity {
 
-    @Id
-    @GeneratedValue(strategy = IDENTITY)
+    @Id @GeneratedValue(strategy = IDENTITY)
     @Column(name ="survey_response_id")
     private Long id;
 
-    private Long userId;
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id")
+    private User user;
+
     // surveyQuestionId 를 Enum 으로 뺄 지, 일단은 안빼놓을게요.
     private Long surveyQuestionId;
 
@@ -28,7 +32,22 @@ public class SurveyResponse extends BaseEntity {
     private String respondentNickname;
     private Long respondentId;
 
+    @Builder
+    public SurveyResponse(User user, Long surveyQuestionId, String response, String respondentNickname, Long respondentId) {
+        this.user = user;
+        this.surveyQuestionId = surveyQuestionId;
+        this.response = response;
+        this.respondentNickname = respondentNickname;
+        this.respondentId = respondentId;
+    }
 
-
-
+    public static SurveyResponse createSurveyResponse(User user, Long surveyQuestionId, String response, String respondentNickname, Long respondentId) {
+        return builder()
+                .user(user)
+                .surveyQuestionId(surveyQuestionId)
+                .response(response)
+                .respondentNickname(respondentNickname)
+                .respondentId(respondentId)
+                .build();
+    }
 }
