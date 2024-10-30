aaff88f6a944ad897b1527ebcbde1df358e7199c
조아름 <a_lme@naver.com>
2024-10-30 13:44:43 +0900

    fix: RequestDto에서 사용하지 않는 값 삭제

--- backend/me-to-you/src/main/java/nodawoon/me_to_you/domain/surveyResponse/presentation/dto/request/SurveyResponseRequest.java
+++ backend/me-to-you/src/main/java/nodawoon/me_to_you/domain/surveyResponse/presentation/dto/request/SurveyResponseRequest.java
@@ -1,15 +1,10 @@
 package nodawoon.me_to_you.domain.surveyResponse.presentation.dto.request;
 
-import java.time.LocalDateTime;
 import java.util.List;
 
 public record SurveyResponseRequest(
-        Long surveyResponseId,
-        Long userId,
         Long surveyQuestionId,
         List<String> response,  // 객관식, 주관식, 단답형 등 다양한 응답 형식이지만 다 json으로 받아옴
-        LocalDateTime createdDate,
-        LocalDateTime lastModifyDate,
         String respondentNickname,
         String respondentId) {
 }
