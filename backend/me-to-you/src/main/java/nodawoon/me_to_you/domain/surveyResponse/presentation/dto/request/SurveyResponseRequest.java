962180d12c3508950e7ba084adc4608ebbacb747
조아름 <a_lme@naver.com>
2024-10-30 12:48:35 +0900

    fix: 응답 받아올 때 중복이 있어서 List로 받아 옴.

--- backend/me-to-you/src/main/java/nodawoon/me_to_you/domain/surveyResponse/presentation/dto/request/SurveyResponseRequest.java
+++ backend/me-to-you/src/main/java/nodawoon/me_to_you/domain/surveyResponse/presentation/dto/request/SurveyResponseRequest.java
@@ -1,12 +1,13 @@
 package nodawoon.me_to_you.domain.surveyResponse.presentation.dto.request;
 
 import java.time.LocalDateTime;
+import java.util.List;
 
 public record SurveyResponseRequest(
         Long surveyResponseId,
         Long userId,
         Long surveyQuestionId,
-        String response,  // 객관식, 주관식, 단답형 등 다양한 응답 형식이지만 다 json으로 받아옴
+        List<String> response,  // 객관식, 주관식, 단답형 등 다양한 응답 형식이지만 다 json으로 받아옴
         LocalDateTime createdDate,
         LocalDateTime lastModifyDate,
         String respondentNickname,
