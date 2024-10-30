33049c1f14c0b41e150bb482e708609b16f77ddd
조아름 <a_lme@naver.com>
2024-10-30 10:35:55 +0900

    fix: 저장 완료 시 응답 수정

--- backend/me-to-you/src/main/java/nodawoon/me_to_you/domain/surveyResponse/presentation/SurveyResponseController.java
+++ backend/me-to-you/src/main/java/nodawoon/me_to_you/domain/surveyResponse/presentation/SurveyResponseController.java
@@ -1,14 +1,12 @@
-package nodawoon.me_to_you.domain.surveyResponse.domain.presentation;
+package nodawoon.me_to_you.domain.surveyResponse.presentation;
 
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.security.SecurityRequirements;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
-import nodawoon.me_to_you.domain.surveyResponse.domain.presentation.dto.request.SurveyResponseRequest;
+import nodawoon.me_to_you.domain.surveyResponse.presentation.dto.request.SurveyResponseRequest;
 import nodawoon.me_to_you.domain.surveyResponse.service.SurveyResponseService;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @Tag(name = "설문 답변", description = "설문 답변 API")
@@ -22,9 +20,9 @@ public class SurveyResponseController {
     @SecurityRequirements
     @Operation(summary = "기본 질문 답변 저장")
     @PostMapping
-    public ResponseEntity<Void> createSurveyResponse(
+    public void createSurveyResponse(
             @RequestBody @Valid SurveyResponseRequest surveyResponseRequest) {
         surveyResponseService.createSurveyResponse(surveyResponseRequest);
-        return ResponseEntity.status(HttpStatus.CREATED).build();
+        // 200
     }
 }
