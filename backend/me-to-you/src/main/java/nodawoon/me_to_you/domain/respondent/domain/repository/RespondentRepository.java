ff9e30b8a26afb66a0a0425aca15e47018171a02
조아름 <a_lme@naver.com>
2024-10-31 03:39:11 +0900

    feat: Respondent(응답자) 추가

---
+++ backend/me-to-you/src/main/java/nodawoon/me_to_you/domain/respondent/domain/repository/RespondentRepository.java
@@ -0,0 +1,7 @@
+package nodawoon.me_to_you.domain.respondent.domain.repository;
+
+import nodawoon.me_to_you.domain.respondent.domain.Respondent;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface RespondentRepository extends JpaRepository<Respondent, Long> {
+}
